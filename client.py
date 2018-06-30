d0752bffec0cab3c27515d8386db8999ce0f0d42
Sarbjit Singh <srbcheema1@gmail.com>
2018-07-01 01:33:56 +0530

    can listen to one client

--- client.py
+++ client.py
@@ -2,20 +2,27 @@
 
 import socket
 
-host="127.0.0.1"
-port=12345
 
 def print_data(data):
     print(data.decode("utf-8"))
 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((host,port))
-
-while True:
+def get_input():
     inp = input()
-    s.sendall(inp.encode('UTF-8'))
-    data=s.recv(1024)
-    print_data(data)
+    return inp.encode('UTF-8')
 
-s.close()
+def create_connection():
+    host="127.0.0.1"
+    port=12345
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((host,port))
+    return s
 
+if (__name__ == "__main__"):
+    s = create_connection()
+    while True:
+        inp = get_input()
+        s.sendall(inp)
+        data=s.recv(1024)
+        print_data(data)
+
+    s.close()
--- server.py
+++ server.py
@@ -2,20 +2,24 @@
 
 import socket
 import sys
+
 host="127.0.0.1"
 port=12345
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((host,port))
-s.listen(1)
+s.listen(5)
 
 def print_data(data):
     print("got",end=' ')
     print(data)
 
+def verify(data):
+    print("verifying ")
 
 conn,addr=s.accept()
 print('Connected by :',addr)
+
 while True:
     data=conn.recv(1024)
     if not data : break
