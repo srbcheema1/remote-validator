b46cea63b1dfff183c236309f1df795131f7c315
Sarbjit Singh <srbcheema1@gmail.com>
2018-06-30 19:49:05 +0530

    basic server client

---
+++ client.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+import socket
+
+host="127.0.0.1"
+port=12345
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((host,port))
+
+s.sendall("Hello Python!".encode('UTF-8'))
+data=s.recv(1024)
+
+print("Recieved data:",data)
+s.close()
+
