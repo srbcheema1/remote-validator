ff305561b8cec75df7a58a7ed0ea43f3ceae6bcc
Sarbjit Singh <srbcheema1@gmail.com>
2018-07-03 16:00:26 +0530

    aws

--- server.py
+++ server.py
@@ -8,10 +8,10 @@ from util.enc_dec import enc, dec
 from util.string_constants import vcf_path
 
 class vcf_server:
-    def __init__(self, port=12345):
+    def __init__(self, host="127.0.0.1", port=12345):
         self.port = port
-        self.hostname = socket.gethostname() # srb-pc
-        self.host = "127.0.0.1"
+        self.hostname = socket.gethostname()
+        self.host = host
         self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # self.ssocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #dono
         self.soc.bind((self.host, self.port))
@@ -60,9 +60,11 @@ class vcf_server:
 if (__name__ == "__main__"):
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port",default="12345", help="PORT number eg:- 12345")
+    parser.add_argument("-a", "--host",default="127.0.0.1", help="HOST ip adress eg:- 127.0.0.1")
     args = parser.parse_args()
 
+    host = args.host
     port = int(args.port)
 
-    remote_validator = vcf_server(port)
+    remote_validator = vcf_server(host,port)
     remote_validator.run()
