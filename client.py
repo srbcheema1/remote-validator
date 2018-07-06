f2e2d24f877a2a6d842e6d80e8b3c5d86cd14f96
Sarbjit Singh <srbcheema1@gmail.com>
2018-07-06 21:59:25 +0530

    wohoo it works

--- client.py
+++ client.py
@@ -24,16 +24,24 @@ else:
 
 def create_iterator():
     while(True):
-        inp = input()
+        try:
+            inp = input()
+            if(int(inp) == 0):
+                break
+        except:
+            print("exiting . . .")
+            break
         inp = message.String(value=inp)
         yield inp
+    yield message.String(value="0")
 
 def receive_output():
     for note in stub.Get_result(message.Empty(),metadata=metadata):
         print(note.value)
 
+def send_input():
+    number_iterator = create_iterator()
+    stub.Validate(number_iterator,metadata=metadata)
 
-threading.Thread(target=receive_output, daemon=True).start()
-
-number_iterator = create_iterator()
-stub.Validate(number_iterator,metadata=metadata)
+threading.Thread(target=send_input, daemon=True).start()
+receive_output()
